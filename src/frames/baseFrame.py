492fdc395c7c217bbe9a9f100f2a68c954718ff5
dood <74739472+itsamedood@users.noreply.github.com>
2024-01-13 17:41:42 -0500

    Rewriting the entire GUI
    It was honestly bad, and after some updates it doesn't even work lol

--- src/frames/baseFrame.py
+++
@@ -1,8 +0,0 @@
-from tkinter import Tk, Frame
-
-
-class BaseFrame(Frame):
-  def __init__(self, master: Tk) -> None:
-    super().__init__(master)
-
-  def build_frame(self) -> Frame: ...  # Replace with widgets.
