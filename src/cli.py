d008cf4191b62c6da534ae524818a59e1eff67b5
itsamedood <itsamedood@gmail.com>
2026-02-19 11:21:10 -0500

    Added Cli code.

--- src/cli.py
+++ src/cli.py
@@ -1,106 +1,74 @@
-# Copyright (C) 2022 David Spencer
-#
-# This file is part of LPM.
-#
-# LPM is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# LPM is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with LPM.  If not, see <http://www.gnu.org/licenses/>.
-
-
-from data.dataManager import DataManager
-from out import LpmError, Special
-from paths import Paths
-from random import randint
+""" Contains the `Cli` class that handles command-line stuff. """
 
 
 class Cli:
-  """Handles the command-line side of LPM."""
+  """
+  Handles command-line stuff like flags & arguments.
+  """
 
-  args: list[str]
-  dm: DataManager
+  VERSION = "0.0.1-dev"  # Initial release. Shit does NOT work yet.
 
-  def __init__(self, _args: list[str]) -> None:
-    self.args = _args
-    self.dm = DataManager(_args[1] if len(_args) > 1 else None, Paths())
+  FLAGS = {
+  """ Easy way to know what flags are set for runtime. """
+    # "help": False,
+    # "version": False,
+    "parent": False,
+    "email": False,
+    "username": False,
+    "password": False,
+    "import": False,
+    "export": False,
+    "plaintext": False,
+    "wipe": False,
+    "reforge": False,
+    "oldkey": False,
+    "newkey": False,
+  }
 
-  def display_help(self): print('\n'.join([
-      "Usage: lpm --flag | command [arg]",
-      "Commands:",
-      "╭─ new",
-      "⏐  get <parent>",
-      "⏐  edit <parent>",
-      "⏐  list",
-      "⏐  search <parent>",
-      "⏐  rm <parent>",
-      "⏐  gen <len>",
-      "⏐  resecure",
-      "⏐  wipe",
-      "⏐  import <path-to-export.txt>",
-      "⏐  export [dc]",
-      "╰─ setup",
-      "Flags:",
-      "╭─ --v",
-      "╰─ --h",
-    ]))
+  HELP = """
+          optional | required
+Usage: lpm [-flags] <action>
+Flags:
+  -help     | -h    Help menu.
+  -version  | -v    Version info.
+  -verbose  | -vb   Verbose output.
+  -parent   | -p    Input parent name.
+  -email    | -e    Input email address.
+  -username | -u    Input username.
+  -password | -pw   Input password.
+  -import   | -im   Import data with the old key. It will be re-encrypted with your current key.
+  -export   | -ex   Export data as-is or to plaintext.
+  -wipe     | -w    Deletes ALL saved data and the key. You will have to set up a new one.
+  -reforge  | -rf   Prompts you with the current key, then for a new one, then re-encrypts everything with the new key.
+"""
 
-  def gen_password(self, _length: str | None) -> str:
-    if _length is None: raise LpmError("missing argument 'length'", 1)
-    length: int | None = None
+  @staticmethod
+  def parse_args(argv: list[str]) -> tuple[list[str], str]:
+    """ Reads flags and the action the user wants to perform. """
 
-    while _length.isdigit():  # All because of what I swear must be a Python3.10+ bug...
-      try: length = int(_length); break
-      except: continue
+    flags = [arg[1:] for arg in argv if arg[0] == '-']  # Strip the '-' from arg.
+    action = argv[-1] if argv[-1][0] != '-' else None
 
-    # global length
+    return flags, action
 
-    # try: length = int(_length)
-    # except: length = None
+  @staticmethod
+  def process_args(flags: list[str]) -> None:
+    """
+    Sets corresponding flags in `Cli.FLAGS` to `True`,
+    or does something based on the flag (like print `Cli.HELP` for `-help` / `-h`.)
+    """
 
-    if length is None: raise LpmError("password length must be an integer", 1)
-    # No need for warning, since it should be obvious that generating a password of 1000+ characters could take a while.
-
-    *chars, = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789`~!@#$%^&*()-_=+[{]};:'\",<.>/?|"
-    return ''.join([chars[randint(0, len(chars)-1)] for _ in range(length)])
-
-  def process_args(self) -> None:
-    command = self.args[1] if len(self.args) > 1 else None
-    flag = self.args[1] if len(self.args) > 1 and (self.args[1][:2] == "--" and len(self.args[1]) > 2) else None
-
-    if flag is not None and len(flag) > 2:
-      match flag[2]:
-        case "v" | "version": print("v0.0.1")
-        case "h" | "help": self.display_help()
-        case _: raise LpmError(f"unknown flag: '{flag}'", 1)
-
-    else:
-      param = self.args[2] if len(self.args) > 2 else None
-
-      match command:
-        case "new": self.dm.new(self.dm.get_data())
-        case "get": self.dm.get(param).print_out()
-        case "edit": self.dm.edit(param)
-        case "list": self.dm.list_sets()
-        case "search": self.dm.search(param)
-        case "rm": self.dm.rm(param)
-
-        # Little oversight, using `out.success` would always add a `.` to the end of the password,
-        # meaning, not only does EVERY password end with a `.`, the password is +1 in length!
-        case "gen": print(f"{Special.SUCCESS}{self.gen_password(param)}{Special.RESET}")
-
-        case "resecure": self.dm.resecure()
-        case "wipe": self.dm.wipe()
-        case "import": self.dm.import_data(param)
-        case "export": self.dm.export(param)
-        case "setup": ...
-
-        case None: self.display_help()
-        case cmd: raise LpmError(f"unknown command: '{cmd}'", 0)
+    for flag in flags:
+      match flag:
+        case "help"     | 'h': print(Cli.HELP)
+        case "version"  | 'v': print(Cli.VERSION)
+        case "verbose"  | 'vb': Cli.FLAGS["verbose"]  = True
+        case "parent"   | 'p': Cli.FLAGS["parent"]    = True
+        case "email"    | 'e': Cli.FLAGS["email"]     = True
+        case "username" | 'u': Cli.FLAGS["username"]  = True
+        case "password" | 'pw': Cli.FLAGS["password"] = True
+        case "import"   | 'i': Cli.FLAGS["import"]    = True
+        case "export"   | 'e': Cli.FLAGS["export"]    = True
+        case "wipe"     | 'w': Cli.FLAGS["wipe"]      = True
+        case "reforge"  | 'r': Cli.FLAGS["reforge"]   = True
+        case _: ...
--- src/main.py
+++ src/main.py
@@ -1,23 +1,8 @@
-# Copyright (C) 2022 David Spencer
-#
-# This file is part of LPM.
-#
-# LPM is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# LPM is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with LPM.  If not, see <http://www.gnu.org/licenses/>.
+""" Entry point. """
+
 
 from cli import Cli
 from sys import argv
 
 if __name__ == "__main__":
-  try: Cli(argv).process_args()
-  except KeyboardInterrupt: print('')
+  flags, action = Cli.parse_args(argv)  # Unpack.
